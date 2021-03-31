0b1b038485dcfb4a5910a6dc6133e22495fbdb12
Bennie Chen <bennchen@cs.stonybrook.edu>
2021-03-31 02:14:41 -0400

    Added Files and Strtok

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -7,13 +7,27 @@
 #include <ctype.h>
 #include <string.h>
 
+
 #include "imprimer.h"
 #include "conversions.h"
 #include "sf_readline.h"
+#include "help.h"
 
 int run_cli(FILE *in, FILE *out)
 {
     // TO BE IMPLEMENTED
-    fprintf(stderr, "You have to implement run_cli() before the application will function.\n");
+	// fprintf(stderr, "You have to implement run_cli() before the application will function.\n");
+    while(1){
+    	char* command = sf_readline("imp> ");
+    	char* token;
+
+    	token = strtok(command, " ");
+
+    	printf("%s\n", command);
+    	printf("%s\n", token);
+        token = strtok(NULL, " ");
+        printf("%s\n", token);
+    }
+
     abort();
 }
---
+++ hw4/src/help.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "imprimer.h"
+#include "conversions.h"
+#include "sf_readline.h"
+
