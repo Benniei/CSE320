c078761e0629375a35626cf1f8e97455f2dcdeba
Bennie Chen <bennchen@cs.stonybrook.edu>
2021-04-01 01:58:18 -0400

    args check

--- hw4/src/cli.c
+++ hw4/src/cli.c
@@ -40,7 +40,7 @@ int run_cli(FILE *in, FILE *out)
         }
         else if(strcmp(token, "quit") == 0){
             if(args_counter > 0){
-                printf("Wrong number of args(given: %d, required: %d) for CLI command \'help\'\n", args_counter, 0);
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'quit\'\n", args_counter, 0);
                 sf_cmd_error("arg count");
             }
             else{
@@ -49,39 +49,67 @@ int run_cli(FILE *in, FILE *out)
             }
         }
         else if(strcmp(token, "type") == 0){
-
+            if(args_counter != 1){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'type\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         /* Confiruration Commands */
         else if(strcmp(token, "printer") == 0){
+            if(args_counter != 2){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'printer\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
 
         }
-        else if(strcmp(token, "conversion") == 0){
-
-        }
-        else if(strcmp(token, "quit") == 0){
-
+        else if(strcmp(token, "conversion") == 0){ //check again
+            if(args_counter != 3){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'conversion\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         /* Informational Commands */
         else if(strcmp(token, "printers") == 0){
-
+            if(args_counter > 0){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'printers\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         else if(strcmp(token, "jobs") == 0){
-
+            if(args_counter > 0){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'jobs\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
-        else if(strcmp(token, "print") == 0){
-
+        else if(strcmp(token, "print") == 0){ //check again
+            if(args_counter != 2){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'print\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         else if(strcmp(token, "cancel") == 0){
-
+            if(args_counter != 1){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'cancel\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         else if(strcmp(token, "pause") == 0){
-
+            if(args_counter != 1){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'pause\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         else if(strcmp(token, "resume") == 0){
-
+            if(args_counter != 1){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'resume\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         else if(strcmp(token, "disable") == 0){
-
+            if(args_counter != 1){
+                printf("Wrong number of args(given: %d, required: %d) for CLI command \'disable\'\n", args_counter, 0);
+                sf_cmd_error("arg count");
+            }
         }
         else if(strcmp(token, "enable") == 0){
 
