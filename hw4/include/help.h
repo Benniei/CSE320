5b11187f05065d2ca8c6c5abd183c14a4170bc83
Bennie Chen <bennchen@cs.stonybrook.edu>
2021-04-02 17:53:48 -0400

    added needed files

--- hw4/include/help.h
+++ hw4/include/help.h
@@ -3,6 +3,9 @@
 #include <ctype.h>
 #include <string.h>
 
+#include "imprimer.h"
+#include "conversions.h"
+#include "sf_readline.h"
 
 /* returns the number of arguments */
 int num_args(char* command);
