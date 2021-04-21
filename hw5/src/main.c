fde871aa9ae39df6159aac9f48ef7233e1d805f5
Bennie Chen <bennchen@cs.stonybrook.edu>
2021-04-21 17:31:24 -0400

    changed the specifications for part 1

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -26,86 +26,28 @@ int main(int argc, char* argv[]){
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
-    int counter = 1; // counters the amount of variables being used
-    char hflag = 0, pflag = 0, dflag = 0, qflag = 0, pdec = 0;
+    int counter = 1; // counters the amount of variables being used;
     char* port;
-    while(*(argv + counter) != NULL){
-        if(counter == argc){
-            printf("USAGE: -h <hostname> (optional), -p <port> (required), -d, -q\n");
-            exit(EXIT_FAILURE);
-        }
-        if(strcmp(*(argv + counter), "-h") == 0){
-            // optional host name
-            if(hflag == 1){
-                printf("Error: h has been declared twice\n");
-                exit(EXIT_FAILURE);
-            }
-            else
-                hflag = 1;
-
-            // printf("-h\n");
-            counter++;
-            if(counter == argc || **(argv + counter) == '-'){
-                // local host will be chosen
-                printf("local host is chosen\n");
-                continue;
-            }
-            printf("Host: %s\n", *(argv + counter));
-            counter++;
-        }
-        else if(strcmp(*(argv + counter), "-p") == 0){
-            // required port number
-            if(pflag == 1){
-                printf("Error: p has been declared twice\n");
-                exit(EXIT_FAILURE);
-            }
-            else
-                pflag = 1;
-
-            // printf("-p\n");
-            counter++;
-            if(counter == argc || **(argv + counter) == '-'){
-                // Invalid port name
-                printf("Error: Invalid port name\n");
-                exit(EXIT_FAILURE);
-            }
-            printf("Portname: %s\n", *(argv + counter));
-            pdec = 1;
-            port = *(argv + counter);
-            counter++;
-        }
-        else if(strcmp(*(argv + counter), "-d") == 0){
-            // debug
-            if(dflag == 1){
-                printf("Error: d has been declared twice\n");
-                exit(EXIT_FAILURE);
-            }
-            else
-                dflag = 1;
-            // printf("-d\n");
-            counter++;
-        }
-        else if(strcmp(*(argv + counter), "-q") == 0){
-            // run without prompting
-            if(qflag == 1){
-                printf("Error: q has been declared twice\n");
-                exit(EXIT_FAILURE);
-            }
-            else
-                qflag = 1;
-
-            // printf("-q\n");
-            counter++;
-        }
-        else{
-            printf("USAGE: -h <hostname> (optional), -p <port> (required), -d, -q\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    if(pdec == 0){
-        printf("Port number was not provided");
+    if(argc != 3){
+        printf("USAGE: -p <port> (required)\n");
         exit(EXIT_FAILURE);
     }
+    if(strcmp(*(argv + counter), "-p") == 0){
+        counter++;
+        if(**(argv + counter) == '-'){
+            // Invalid port name
+            printf("Error: Invalid port name\n");
+            exit(EXIT_FAILURE);
+        }
+        printf("Portname: %s\n", *(argv + counter));
+        port = *(argv + counter);
+        counter++;
+    }
+    else{
+        printf("USAGE: -p <port> (required)\n");
+        exit(EXIT_FAILURE);
+    }
+
     // Perform required initializations of the client_registry and
     // player_registry.
     user_registry = ureg_init();
