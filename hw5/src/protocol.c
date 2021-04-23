f60fd5be76614fd83cb9d7469ff0822047fda597
Bennie Chen <bennchen@cs.stonybrook.edu>
2021-04-23 14:35:16 -0400

    finished protocol

--- hw5/src/protocol.c
+++ hw5/src/protocol.c
@@ -16,13 +16,39 @@
 #include "csapp.h"
 
 int proto_send_packet(int fd, CHLA_PACKET_HEADER *hdr, void *payload){
-	printf("proto_send_packet\n");
-	printf("File descriptor: %d\n", fd);
+	// fprintf(stderr, "send packet\n");
+	if(rio_writen(fd, hdr, sizeof(CHLA_PACKET_HEADER)) < 0){
+		debug("Send Packet Failed [Interrupt]\n");
+		// set errno
+		return -1;
+	}
+	if(hdr->payload_length > 0){
+		if(rio_writen(fd, payload, hdr->payload_length) < 0){
+			debug("Send Packet Failed [Interrupt]\n");
+			// set errno
+			return -1;
+		}
+	}
 	return 0;
 }
 
 int proto_recv_packet(int fd, CHLA_PACKET_HEADER *hdr, void **payload){
-	printf("proto_recv_packet\n");
-	printf("File descriptor: %d\n", fd);
+	// fprintf(stderr, "recieve packet\n");
+	if(rio_readn(fd, hdr, sizeof(CHLA_PACKET_HEADER)) < 0){
+		debug("Recieve Packet Failed [Interrupt]\n");
+		// set errno
+		return -1;
+	}
+	if(hdr->payload_length > 0){
+		uint32_t hbo = ntohl(hdr->payload_length);
+		//fprintf(stderr, "down here %d\n", hbo);
+		*payload = malloc(hbo);
+		if(read(fd, *payload, hbo) <= 0){
+			debug("Recieve Packet Failed [Interrupt]\n");
+			// set errno
+			return -1;
+		}
+		fprintf(stderr, "payload: %s", (char*)*payload);
+	}
 	return 0;
 }
